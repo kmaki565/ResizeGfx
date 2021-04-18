a08ebc8d2feb932acdd551dd844e2e03acb1fcdf
Koutaroh Maki <kmaki565@gmail.com>
2021-04-18 22:50:58 +0900

    Add project files.

---
+++ ResizeGfx/Main.cpp
@@ -0,0 +1,69 @@
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include "CommonTypes.h"
+#include "WICTextureLoader.h"
+
+
+//
+// Globals
+//
+//OUTPUTMANAGER OutMgr;
+
+// Below are lists of errors expect from Dxgi API calls when a transition event like mode change, PnpStop, PnpStart
+// desktop switch, TDR or session disconnect/reconnect. In all these cases we want the application to clean up the threads that process
+// the desktop updates and attempt to recreate them.
+// If we get an error that is not on the appropriate list then we exit the application
+
+// These are the errors we expect from general Dxgi API due to a transition
+HRESULT SystemTransitionsExpectedErrors[] = {
+                                                DXGI_ERROR_DEVICE_REMOVED,
+                                                DXGI_ERROR_ACCESS_LOST,
+                                                static_cast<HRESULT>(WAIT_ABANDONED),
+                                                S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIOutput1::DuplicateOutput due to a transition
+HRESULT CreateDuplicationExpectedErrors[] = {
+                                                DXGI_ERROR_DEVICE_REMOVED,
+                                                static_cast<HRESULT>(E_ACCESSDENIED),
+                                                DXGI_ERROR_UNSUPPORTED,
+                                                DXGI_ERROR_SESSION_DISCONNECTED,
+                                                S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIOutputDuplication methods due to a transition
+HRESULT FrameInfoExpectedErrors[] = {
+                                        DXGI_ERROR_DEVICE_REMOVED,
+                                        DXGI_ERROR_ACCESS_LOST,
+                                        S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+// These are the errors we expect from IDXGIAdapter::EnumOutputs methods due to outputs becoming stale during a transition
+HRESULT EnumOutputsExpectedErrors[] = {
+                                          DXGI_ERROR_NOT_FOUND,
+                                          S_OK                                    // Terminate list with zero valued HRESULT
+};
+
+
+int wmain(int argc, wchar_t* argv[])
+{
+	std::vector<std::wstring> params(argv, argv + argc);
+
+	if (params.size() < 2)
+	{
+		wprintf(L"Usage: %s <filepath>\n", params[0].c_str());
+		return 1;
+	}
+
+	ID3D11ShaderResourceView* hpShaderResourceView = NULL;
+	ID3D11Resource* hpResource = NULL;
+	HRESULT hr;
+
+	hr = DirectX::CreateWICTextureFromFile(nullptr, L"file", &hpResource, &hpShaderResourceView);
+
+	if (FAILED(hr)) {
+		
+	}
+	return 0;
+}
